45812c12b103b4d74873b332c0ff024129b3e542
alexiscatnip <alexiscatnip@gmail.com>
2022-02-06 13:06:57 +0800

    Added: Global access to main thread reference

---
+++ Assets/Raindrop/Bootstrap/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace Raindrop.Services.Bootstrap
+{
+    public class Globals
+    {
+        public static Thread GMainThread;
+        
+        public static bool isOnMainThread()
+        {
+            return Globals.GMainThread.Equals(System.Threading.Thread.CurrentThread);
+        }
+    }
+}
