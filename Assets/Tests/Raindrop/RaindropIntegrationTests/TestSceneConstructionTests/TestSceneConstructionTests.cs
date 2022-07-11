3a7323960e2d371288e268420de7ff1c2944381d
alexiscatnip <alexiscatnip@gmail.com>
2022-07-11 21:48:01 +0800

    rename TestSceneConstructionTests to HeadlessScene_ConstructionTests

--- Assets/Tests/Raindrop/RaindropIntegrationTests/TestSceneConstructionTests/TestSceneConstructionTests.cs
+++ Assets/Tests/Raindrop/RaindropIntegrationTests/TestSceneConstructionTests/TestSceneConstructionTests.cs
@@ -3,13 +3,13 @@ using NUnit.Framework;
 using Raindrop.Tests.RaindropIntegrationTests.Helpers;
 using UnityEngine.TestTools;
 
-namespace Raindrop.Tests.RaindropIntegrationTests.TestSceneConstructionTests
+namespace Raindrop.Tests.RaindropIntegrationTests.HeadlessScene
 {
     //test that it is possible to construct the test scene.
     //the test scene contains no UI, but has a bootstrapped raindropinstance
     [TestFixture()]
 
-    public class TestSceneConstructionTests
+    public class HeadlessScene_ConstructionTests
     {
         [UnityTest]
         public IEnumerator SceneLoading_Bootstrapper_IsSuccessful()
