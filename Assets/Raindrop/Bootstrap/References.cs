b344445fe36a765db6064275c75f2b6c5f1d1595
alexiscatnip <alexiscatnip@gmail.com>
2022-07-19 00:10:56 +0800

    refactor: move MainThreadDispatcher to base bootstrapper

--- Assets/Raindrop/Bootstrap/References.cs
+++ Assets/Raindrop/Bootstrap/References.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 namespace Raindrop.Bootstrap
 {
-    //this class is in the main scene, where you can give it references to monobehaviors in order to inject them.
-    // of course, runtime- created monobehaviors cannot be injected this way, so you will need to find some other way for those.
-    public class References : Singleton<References>
+    //this monobehavior works like a control panel - you plug each major UI
+    //component into each slot.
+    // of course, monobehaviors created at runtime cannot be plugged-in
+    // this way, so you will need to find some other way for those.
+    public class References : MonoBehaviour
     {
         [SerializeField] public ModalsManager mm;
         [SerializeField] public ScreenStackManager sm;
